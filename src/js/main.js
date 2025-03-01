ad4aec2973f3976da5b40ca93e6b49bbc09fec21
Lareta064 <lareta1@yandex.ru>
2025-03-01 22:50:14 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let bodyEl = document.body;
     let headerBlock = document.querySelector('header.header');
     const hasDropItem = document.querySelector('#has-drop');
+   
     // HEADER SUBMENU
     if(hasDropItem){
       const dropMenu =  document.querySelector('#drop-menu');
@@ -15,5 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
             headerBlock.classList.add('show-after');
           }
       });
+    }
+
+    // MOBILE MENU
+    const mobileMenuIcon = document.querySelector('#menu-toggle');
+    const mobileMenuBlock = document.querySelector('#moble-menu');
+    
+    if(mobileMenuIcon){
+      mobileMenuIcon.addEventListener('click', ()=>{
+        if(mobileMenuIcon.classList.contains('active')){
+          mobileMenuIcon.classList.remove('active');
+          mobileMenuBlock.classList.remove('active');
+          bodyEl.classList.remove('lock');
+        }else{
+          mobileMenuIcon.classList.add('active');
+          mobileMenuBlock.classList.add('active');
+          bodyEl.classList.add('lock');
+        }
+      });
+     
     }
 });
